72c5721a6531ca41c7b005402ab392a2dad585ca
Vytenis Kučiauskas <vytenis.kuciauskas@nfq.lt>
2017-12-17 14:01:11 +0200

    ci(travis): Add CI
    Added travis as CI for publishing to NPM

---
+++ typings/index.d.ts
@@ -0,0 +1,22 @@
+declare module 'react-router4-with-layouts/index' {
+    /// <reference types="react" />
+    import * as React from 'react';
+    import * as PropTypes from 'prop-types';
+    import { RouteProps } from 'react-router-dom';
+    export interface IRouteProps {
+        layout?: React.ComponentType<RouteProps> | React.ComponentType<any>;
+        component: React.ComponentType<RouteProps> | React.ComponentType<any>;
+    }
+    export class Route extends React.Component<IRouteProps & RouteProps, {}> {
+        static propTypes: {
+            layout: PropTypes.Requireable<any>;
+            component: PropTypes.Validator<any>;
+        };
+        render(): JSX.Element;
+    }
+
+}
+declare module 'react-router4-with-layouts' {
+    import main = require('react-router4-with-layouts/index');
+    export = main;
+}
