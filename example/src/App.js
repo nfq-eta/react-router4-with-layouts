03c624ae67d20e705d7a1c4473a1090a95fbc51a
Vytenis Kučiauskas <vytenis.kuciauskas@nfq.lt>
2017-12-17 15:38:24 +0200

    deploy demo page

--- example/src/App.js
+++ example/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router4-with-layouts';
-import { Router, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 
 import { DefaultLayout } from './layouts/DefaultLayout';
@@ -13,7 +13,7 @@ const history = createBrowserHistory();
 export default class App extends Component {
   render () {
     return (
-      <Router history={history}>
+      <Router history={history} basename={`${process.env.PUBLIC_URL}`}>
         <Switch>
           <Route path="/" component={HomePage} exact layout={DefaultLayout} />
           <Route path="/login" component={LoginPage} layout={EmptyLayout} />
--- example/src/components/MenuComponent.js
+++ example/src/components/MenuComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 import styles from './MenuComponent.css';
 
@@ -7,7 +7,6 @@ export class MenuComponent extends Component {
   render() {
     return (
       <div className={styles.menu}>
-        <BrowserRouter basename="/react-router4-with-layouts"/>
         <NavLink to="/" >Home</NavLink>
         <NavLink to="/login" >Login</NavLink>
       </div>
