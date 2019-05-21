b573f67d9b72d2bd73daa3b996211c425c9987ea
Vytenis Kuciauskas <projektas@gmail.com>
2019-05-21 13:32:39 +0300

    fix(PropTypes): Fixed prop types, no can be extended

--- example/src/App.js
+++ example/src/App.js
@@ -1,24 +1,21 @@
 import React, { Component } from 'react';
 import { Route } from 'react-router4-with-layouts';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
-import { createBrowserHistory } from 'history';
+import { BrowserRouter, Switch } from 'react-router-dom';
 
 import { DefaultLayout } from './layouts/DefaultLayout';
 import { HomePage } from './pages/HomePage';
 import { LoginPage } from './pages/LoginPage';
 import { EmptyLayout } from './layouts/EmptyLayout';
 
-const history = createBrowserHistory();
-
 export default class App extends Component {
   render () {
     return (
-      <Router history={history} basename={`${process.env.PUBLIC_URL}`}>
+      <BrowserRouter basename={`${process.env.PUBLIC_URL}`}>
         <Switch>
           <Route path="/" component={HomePage} exact layout={DefaultLayout} />
           <Route path="/login" component={LoginPage} layout={EmptyLayout} />
         </Switch>
-      </Router>
+      </BrowserRouter>
     );
   }
 }
